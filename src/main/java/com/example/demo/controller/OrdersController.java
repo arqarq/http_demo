484d407866e1c7e67507bcde3b49507443f21eeb
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-13 15:12:41 +0100

    - OrdersController c.d.

--- src/main/java/com/example/demo/controller/OrdersController.java
+++ src/main/java/com/example/demo/controller/OrdersController.java
@@ -1,5 +1,28 @@
 package com.example.demo.controller;
 
-public class OrdersController {
+import com.example.demo.model.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+public class OrdersController {
+    private List<Order> orders = new LinkedList<>();
+
+    @GetMapping("/orders")
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    @PostMapping("/orders")
+    public ResponseEntity<?> addOrder(@RequestBody Order order) {
+        orders.add(order);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
