5164622ab4a9487122a736dc6ff51a2cf52295fe
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-13 13:46:10 +0100

    - GreetingController improved with AtomicLong

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class GreetingController {
-    private int counter = 0;
+    private AtomicLong counter = new AtomicLong(); // zabezpieczenie wielowątkowości
 
     @GetMapping("/greeting")
     public Greeting greet(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter++, "Hello " + name);
+        return new Greeting(counter.incrementAndGet(), "Hello " + name);
     }
 }
